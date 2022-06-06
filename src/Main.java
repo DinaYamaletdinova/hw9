0d309896dd39a10bfb322cd597dde024c4ca21a2
79273 <destal95@gmail.com>
2022-06-07 02:28:47 +0500

    second commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Author author1 = new Author("John", "Tolkien");
+        Author author2 = new Author("Howard", "Lovecraft");
+
+        Book book1 = new Book("The Hobbit, or There and Back Again", author1, 1937);
+        Book book2 = new Book("At the Mountains of Madness", author2, 1936);
+
+        System.out.println(book1.getAuthor().getFullName()+ " " + book1.getTitle()+" "+book1.getYear());
+        System.out.println(book2.getAuthor().getFullName()+ " " + book2.getTitle()+" "+book2.getYear());
     }
+
 }
